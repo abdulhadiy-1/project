2d6ee7ad048cc55267e74df5696cf4c38997e8cc
abdulhadiy-1 <hadiyhadiy2008@gmail.com>
2025-05-09 16:54:45 +0500

    a

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,14 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateProductDto {
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   name: string;
   @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
   categoryId: number;
   @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
   restaurantId: number;
   @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
   price: number;
   @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
   isActive: boolean;
 }
--- src/withdraw/dto/create-withdraw.dto.ts
+++ src/withdraw/dto/create-withdraw.dto.ts
@@ -1,13 +1,17 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Type } from "@prisma/client";
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from '@prisma/client';
+import { IsNumber } from 'class-validator';
 
 export class CreateWithdrawDto {
-    @ApiProperty({ example: 1})
-    orderId: number;
-    @ApiProperty({ example: 1})
-    restaurantId: number;
-    @ApiProperty({ enum:Type})
-    type: Type;
-    @ApiProperty({ example: 12345})
-    sum: number;
+  @ApiProperty({ example: 1 })
+  @IsNumber()
+  orderId: number;
+  @ApiProperty({ example: 1 })
+  @IsNumber()
+  restaurantId: number;
+  @ApiProperty({ enum: Type })
+  type: Type;
+  @ApiProperty({ example: 12345 })
+  @IsNumber()
+  sum: number;
 }
